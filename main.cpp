e90158b5ed22b97eb5cc4685d02081ba9e38f167
dinoplane <isacsac2017@gmail.com>
2024-03-27 16:43:04 -0700

    fixed viewport

--- main.cpp
+++ main.cpp
@@ -337,11 +337,6 @@ private:
         VkPipelineViewportStateCreateInfo viewportState{};
         viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
         viewportState.viewportCount = 1;
-        viewportState.scissorCount = 1;
-
-        VkPipelineViewportStateCreateInfo viewportState{};
-        viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
-        viewportState.viewportCount = 1;
         viewportState.pViewports = &viewport;
         viewportState.scissorCount = 1;
         viewportState.pScissors = &scissor;
